c4ac30e3f5949dcea0ee29cbb2534f0c2b3d1396
Dominique Zimmermann <dzim@gmx.net>
2020-11-10 11:27:46 +0100

    Day 26 - Exercise 2 - Rails stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,14 +2,28 @@ class QuestionsController < ApplicationController
   def ask
   end
 
+  # def answer
+  #   @answers = [ 'Great!', 'Silly question, get dressed and go to work!', 'I dont care, get dressed and go to work!' ]
+  #   if params[:question] == 'I am going to work'
+  #     @answer = @answers[0]
+  #   elsif params[:question].include?('?')
+  #     @answer = @answers[1]
+  #   else
+  #     @answer = @answers[2]
+  #   end
+  # end
+
   def answer
-    @answers = [ 'Great!', 'Silly question, get dressed and go to work!', 'I dont care, get dressed and go to work!' ]
-    if params[:question] == 'I am going to work'
-      @answer = @answers[0]
-    elsif params[:question].include?('?')
-      @answer = @answers[1]
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
     else
-      @answer = @answers[2]
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
+
 end
